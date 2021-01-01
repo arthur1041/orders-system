81b2dd964053ed628978648a64ffe5c1653853dc
Arthur Cruz <arthur.silva1041@gmail.com>
2021-01-01 00:34:04 -0300

    Projeto criado

---
+++ src/main/java/com/arthurcruz/orderssystem/OrdersSystemApplication.java
@@ -0,0 +1,13 @@
+package com.arthurcruz.orderssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthurcruz/orderssystem/OrdersSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurcruz.orderssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
