9fb4920633fabd948cfa272ff8c92cd3a78e9444
Arthur Cruz <arthur.silva1041@gmail.com>
2021-01-01 11:02:54 -0300

    Criando uma operação de instanciação

--- src/main/java/com/arthurcruz/orderssystem/OrdersSystemApplication.java
+++ src/main/java/com/arthurcruz/orderssystem/OrdersSystemApplication.java
@@ -1,13 +1,34 @@
 package com.arthurcruz.orderssystem;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class OrdersSystemApplication {
+import com.arthurcruz.orderssystem.domain.Category;
+import com.arthurcruz.orderssystem.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class OrdersSystemApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersSystemApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Esportes");
+		Category cat2 = new Category(null, "Roupas");
+	
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 
 }
