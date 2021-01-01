8b570bf6050bedbbb80ec92db5fdabeb9caaf6c9
Arthur Cruz <arthur.silva1041@gmail.com>
2021-01-01 10:44:08 -0300

    Repository e Service da Categoria criados

---
+++ src/main/java/com/arthurcruz/orderssystem/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.arthurcruz.orderssystem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.arthurcruz.orderssystem.domain.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+	
+}
---
+++ src/main/java/com/arthurcruz/orderssystem/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.arthurcruz.orderssystem.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arthurcruz.orderssystem.domain.Category;
+import com.arthurcruz.orderssystem.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public Category find(Integer id) {
+		Optional<Category> categoryObj = repository.findById(id);
+		return categoryObj.orElse(null);
+	}
+	
+}
