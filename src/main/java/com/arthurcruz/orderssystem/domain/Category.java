571a1518c5dd6c0e51ec0ee6ea9123eb53c7a18e
Arthur Cruz <arthur.silva1041@gmail.com>
2021-01-01 02:20:40 -0300

    Banco de dados H2 e criação automática do banco de dados a partir dos objetos

--- src/main/java/com/arthurcruz/orderssystem/domain/Category.java
+++ src/main/java/com/arthurcruz/orderssystem/domain/Category.java
@@ -2,10 +2,20 @@ package com.arthurcruz.orderssystem.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@Column
 	private String name;
 	
 	public Category() {
